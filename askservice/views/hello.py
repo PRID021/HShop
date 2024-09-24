b2a77207f42daa65c4ecc5646d00c1322f2c2a8a
hoang.pham <hoang.pham@executionlab.asia>
2024-09-24 17:29:10 +0700

    end of day

--- askservice/views/hello.py
+++ askservice/views/hello.py
@@ -1,7 +1,9 @@
-from requests import Response
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class HelloWorld(APIView):
+    permission_classes = []
+
     def get(self, request):
         return Response({"message": "Hello, world!"})
