2a6488af450109e6c4f4a576b517a2e259934dc7
DucHoang-UIT <hoangduc.uit.dev@gmail.com>
2024-10-07 19:52:31 +0700

    fix permision missing, send mail

--- app/admins/client_user_admin.py
+++ app/admins/client_user_admin.py
@@ -9,84 +9,70 @@ from app.modules.auth.models.client_user import ClientUser
 from app.tasks.email import send_email_job
 
 
-class ClientUserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput, label="Password")
-    password_confirmation = forms.CharField(
-        widget=forms.PasswordInput, label="Confirm Password"
-    )
+# class ClientUserForm(forms.ModelForm):
+#     password = forms.CharField(widget=forms.PasswordInput, label="Password")
+#     password_confirmation = forms.CharField(
+#         widget=forms.PasswordInput, label="Confirm Password"
+#     )
 
-    class Meta:
-        model = ClientUser
-        fields = [
-            "name",
-            "email",
-            "password",
-            "phone_number",
-        ]
+#     class Meta:
+#         model = ClientUser
+#         fields = [
+#             "name",
+#             "email",
+#             "password",
+#             "phone_number",
+#         ]
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.instance and self.instance.pk:
-            self.fields["password_confirmation"].required = False
-            self.fields["password"].required = False
-            self.fields["password_confirmation"].widget = forms.HiddenInput()
-            self.fields["password"].widget = forms.HiddenInput()
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         if self.instance and self.instance.pk:
+#             self.fields["password_confirmation"].required = False
+#             self.fields["password"].required = False
+#             self.fields["password_confirmation"].widget = forms.HiddenInput()
+#             self.fields["password"].widget = forms.HiddenInput()
 
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get("password")
-        password_confirmation = cleaned_data.get("password_confirmation")
-        if self.instance.pk is None:
-            if password and password_confirmation and password != password_confirmation:
-                raise forms.ValidationError("Passwords do not match.")
-        return cleaned_data
+#     def clean(self):
+#         cleaned_data = super().clean()
+#         password = cleaned_data.get("password")
+#         password_confirmation = cleaned_data.get("password_confirmation")
+#         if self.instance.pk is None:
+#             if password and password_confirmation and password != password_confirmation:
+#                 raise forms.ValidationError("Passwords do not match.")
+#         return cleaned_data
 
 
 class ClientUserAdmin(UserAdmin):
     list_per_page = 30
-    form = ClientUserForm
-    list_display = ("id", "name", "email", "phone_number", "created_at", "updated_at")
-    readonly_fields = ("created_at", "updated_at")
 
-    fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        (
-            ("Personal info"),
-            {"fields": ("first_name", "last_name", "email", "phone_number")},
-        ),
-        (
-            ("Permissions"),
-            {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
-                ),
-            },
-        ),
-        (("Important dates"), {"fields": ("last_login", "date_joined")}),
-    )
+    def save_form(self, request, form, change):
+        # Access raw password in save_form
+        raw_password = form.cleaned_data.get("password1")
+
+        # Create or modify the instance returned by save_form
+        obj = super().save_form(request, form, change)
+
+        # Attach raw_password as a custom attribute to the user object
+        if raw_password:
+            obj.raw_password = raw_password
+
+        return obj
 
     def save_model(
         self, request: HttpRequest, obj: ClientUser, form: forms.ModelForm, change: bool
     ) -> None:
-        raw_password = obj.password
-        if change:
-            current_user = ClientUser.objects.get(pk=obj.pk)
-            self.password = current_user.password
-        else:
-            obj.password = make_password(obj.password)
+
+        # Access the raw_password in save_model, if it was set in save_form
+        raw_password = getattr(obj, "raw_password", None)
+
+        # Save the user
         super().save_model(request, obj, form, change)
 
-        if not change:
+        # If it's a new user, send an email with the raw password
+        if not change and raw_password:
             send_email_job.delay(
                 "[Adventure] Password",
                 f"Thank you for using our application. Your password to login is: {raw_password}",
                 config("FROM_MAIL"),
-                [obj.email],
+                [obj.username],
             )
-
-    def has_delete_permission(self, request, obj=None):
-        return True
--- app/modules/auth/models/client_user.py
+++ app/modules/auth/models/client_user.py
@@ -3,40 +3,11 @@ from django.db import models
 
 
 class ClientUser(AbstractUser):
-    username = models.CharField(max_length=150, unique=True, default="")
-    email = models.CharField(max_length=100, unique=True)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=100, null=True, blank=True)
 
-    # Override the default related names for groups and user_permissions
-    groups = models.ManyToManyField(
-        Group,
-        related_name="clientuser_set",  # Change this to avoid clashes
-        blank=True,
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name="clientuser_set",  # Change this to avoid clashes
-        blank=True,
-    )
 
     def __str__(self):
-        if not self.name:
-            return f"{self.first_name} {self.last_name}".strip()
-        return self.name
 
-    def save(self, *args, **kwargs):
-        # Set the name to first_name + last_name if name is not provided
-        if not self.name:
-            self.name = f"{self.first_name} {self.last_name}".strip()
-        super().save(*args, **kwargs)
-
-    class Meta:
-        verbose_name_plural = "Client Users"
-        permissions = [
-            ("can_vote", "Can vote for a choice"),
-            ("change_task_status", "Can change the status of tasks"),
-            ("close_task", "Can remove a task by setting its status as closed"),
-        ]
+        return self.username
